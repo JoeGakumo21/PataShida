03f5fc5ff0b65dddb8637e4ec54b1f6bf7f06677
JoeGakumo21 <joegakumo1@gmail.com>
2021-11-15 16:18:25 +0300

    add routes to routing module

--- patashida/src/app/app-routing.module.ts
+++ patashida/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AgroproductComponent } from './agroproduct/agroproduct.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
 
-  {path:'about',component:AboutComponent}
+  {path:'about',component:AboutComponent},
+  {path: 'login' ,component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'agroproduct',component:AgroproductComponent},
+  {path:'contactus',component:ContactusComponent},
+  {path:'home',component:HomeComponent}
+
 
 ];
 
